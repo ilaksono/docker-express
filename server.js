549394b801036751da8287a2b2ed9153217bc7be
ilaksono <60628041+ilaksono@users.noreply.github.com>
2020-12-24 21:53:42 -0500

    update for gcloud

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const app = require('express')();
 
 
 app.get('/', (req, res) => {
-  res.send('Hello there!');
+  res.send('Hello there sir');
 })
 
 app.listen(3001, () => {
   console.log('running on 3001');
+})
+
+app.get('/users', (req, res) => {
+  res.send({name: 'Ian', id: 1});
 })
